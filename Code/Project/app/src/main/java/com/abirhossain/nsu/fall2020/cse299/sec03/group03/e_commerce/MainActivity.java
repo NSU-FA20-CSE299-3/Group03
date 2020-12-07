8c96728f791253b2caa88788080394129023b237
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-08 01:12:13 +0600

    Enhancement #12  Login Activity
    Designed Login activity

--- Code/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/e_commerce/MainActivity.java
+++ Code/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/e_commerce/MainActivity.java
@@ -2,7 +2,9 @@ package com.abirhossain.nsu.fall2020.cse299.sec03.group03.e_commerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +17,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         signUpNowButton = (Button)findViewById(R.id.get_started_Signup);
         loginNowButton = (Button)findViewById(R.id.get_started_login);
+        loginNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginActivityIntent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(loginActivityIntent);
+            }
+        });
 
-        
     }
 }
