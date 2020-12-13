2550761cbb4f8a311455b19e1cb1f842e97e5127
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-13 20:39:06 +0600

    Enhancement #12 : Login Activity
    Created private variables for getting inputs from user and clicking on login button

--- Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/LoginActivity.java
+++ Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/LoginActivity.java
@@ -3,9 +3,14 @@ package com.abirhossain.nsu.fall2020.cse299.sec03.group03.ShopOnline;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText InputNumber, InputPassword;
+    private Button LoginNtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
