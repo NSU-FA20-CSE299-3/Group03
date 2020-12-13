e14236a27b9c8140f4bf9e5fbef2fecbcc01a5ca
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-13 20:45:34 +0600

    Enhancement #12 : Login Activity
    Mapped Priavate variables using findViewById. And created new  progressDialog variable loadingBar

--- Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/LoginActivity.java
+++ Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/LoginActivity.java
@@ -2,6 +2,7 @@ package com.abirhossain.nsu.fall2020.cse299.sec03.group03.ShopOnline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,11 +11,22 @@ import android.widget.EditText;
 public class LoginActivity extends AppCompatActivity {
 
     private EditText InputNumber, InputPassword;
-    private Button LoginNtn;
+    private Button LoginBtn;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        InputNumber = findViewById(R.id.login_phone_number_input);
+        InputPassword = findViewById(R.id.login_password_input);
+        LoginBtn = findViewById(R.id.Login_Page_login);
+        loadingBar = new ProgressDialog(this);
+
+        
+
+
+
+
     }
 }
