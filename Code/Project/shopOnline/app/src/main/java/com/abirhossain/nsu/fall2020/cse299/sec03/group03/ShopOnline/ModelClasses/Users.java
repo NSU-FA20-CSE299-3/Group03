9ae007510014bc9a74c22effe54188373de7f2ce
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-18 18:26:42 +0600

    Enhancement #12 Login Actvity
    Generated constructor getter and setter

--- Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/ModelClasses/Users.java
+++ Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/ModelClasses/Users.java
@@ -1,6 +1,39 @@
 package com.abirhossain.nsu.fall2020.cse299.sec03.group03.ShopOnline.ModelClasses;
 
 public class Users {
-    
+    private String name,password,phone;
+    public Users(){
 
+
+    }
+
+    public Users(String name, String password, String phone) {
+        this.name = name;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
