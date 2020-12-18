ab873c6f1b764586beb0543c9e34ea57d61f5980
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-18 18:36:31 +0600

    Enhancement #12 New pacakag and class created
    created new pacakage prevalent and inside that created a new prevalent class

---
+++ Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/Prevalent/Prevalent.java
@@ -0,0 +1,9 @@
+package com.abirhossain.nsu.fall2020.cse299.sec03.group03.ShopOnline.Prevalent;
+
+import com.abirhossain.nsu.fall2020.cse299.sec03.group03.ShopOnline.ModelClasses.Users;
+
+public class Prevalent {
+
+    public static Users onlineUsers;
+    
+}
