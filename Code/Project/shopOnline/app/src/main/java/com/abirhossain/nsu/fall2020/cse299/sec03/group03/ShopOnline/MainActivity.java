29b00f120916c08f418d8e7a9535133135eccb7f
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-22 19:19:22 +0600

    Enhancement #18 : Checking credential if remember me was checked
    Checking credemtials from allowaccess method if remember me was checked

--- Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/MainActivity.java
+++ Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/MainActivity.java
@@ -1,20 +1,30 @@
 package com.abirhossain.nsu.fall2020.cse299.sec03.group03.ShopOnline;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.abirhossain.nsu.fall2020.cse299.sec03.group03.ShopOnline.ModelClasses.Users;
 import com.abirhossain.nsu.fall2020.cse299.sec03.group03.ShopOnline.Prevalent.Prevalent;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import io.paperdb.Paper;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button signUpNowButton, loginNowButton;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         signUpNowButton = (Button)findViewById(R.id.get_started_Signup);
         loginNowButton = (Button)findViewById(R.id.get_started_login);
+        loadingBar = new ProgressDialog(this);
 
         Paper.init(this);
 
@@ -47,13 +58,47 @@ public class MainActivity extends AppCompatActivity {
 
         if(UserPhoneKey!="" && UserPasswordKey !=""){
             if (!TextUtils.isEmpty(UserPhoneKey) && !TextUtils.isEmpty(UserPasswordKey)){
-                AllowAccess(UserPasswordKey,UserPasswordKey);
+                AllowAccess(UserPhoneKey,UserPasswordKey);
             }
         }
 
     }
 
-    private void AllowAccess(String userPasswordKey, String userPasswordKey1) {
+    private void AllowAccess(final String phone, final String password) {
+
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.child("Users").child(phone).exists()){
+
+                    Users userData = snapshot.child("Users").child(phone).getValue(Users.class);
+                    if (userData.getPhone().equals(phone)){
+                        if (userData.getPassword().equals(password)){
+                            Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+                            Intent loginActivityIntent = new Intent(MainActivity.this, HomeActivity.class);
+                            startActivity(loginActivityIntent);
+                        }
+                        else {
+
+                            loadingBar.dismiss();
+                            Toast.makeText(MainActivity.this, "Incorrect Password", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Account with this number doesn't exist", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
 
