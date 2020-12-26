590ee9861c375c0979f59820cf0729394088f044
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-26 22:44:21 +0600

    Enhancement #23 Redirect to activity
    After successful login admin will enter into ProductCatagoryAdmin activity

--- Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/LoginActivity.java
+++ Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/LoginActivity.java
@@ -130,7 +130,7 @@ public class LoginActivity extends AppCompatActivity {
                                 Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
                                 loadingBar.dismiss();
 
-                                Intent AdminLoginActivityIntent = new Intent(LoginActivity.this, AdminAddNewProduct.class);
+                                Intent AdminLoginActivityIntent = new Intent(LoginActivity.this, ProductCatagoryAdmin.class);
                                 startActivity(AdminLoginActivityIntent);
 
                             }
