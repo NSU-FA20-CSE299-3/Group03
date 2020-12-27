707a6d7e6400b30a6c0de5052979c77455cb762a
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-27 19:17:22 +0600

    Enhancement #20 : Updated AdminAddNewProduct.java file
    Added some variables and connected them with the ids that were declared in the xml file

--- Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/AdminAddNewProduct.java
+++ Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/AdminAddNewProduct.java
@@ -3,11 +3,17 @@ package com.abirhossain.nsu.fall2020.cse299.sec03.group03.ShopOnline;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AdminAddNewProduct extends AppCompatActivity {
 
     private String selectedCategory;
+    private EditText product_name,product_desc, product_price;
+    private ImageView product_img;
+    private Button addBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,15 @@ public class AdminAddNewProduct extends AppCompatActivity {
         setContentView(R.layout.activity_admin_add_new_product);
 
         selectedCategory= getIntent().getExtras().get("category").toString();
-        Toast.makeText(AdminAddNewProduct.this, selectedCategory, Toast.LENGTH_SHORT).show();
+        product_name= findViewById(R.id.NewProductName);
+        product_desc= findViewById(R.id.NewProductDesc);
+        product_price = findViewById(R.id.NewProductPrice);
+        product_img = findViewById(R.id.NewProductImg);
+        addBtn = findViewById(R.id.NewProductAddBtn);
+
+
+
+
+
     }
 }
