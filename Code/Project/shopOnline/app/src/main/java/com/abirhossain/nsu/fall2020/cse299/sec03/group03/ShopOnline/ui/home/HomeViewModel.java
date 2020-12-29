7939e3b9ec729c5917cc0c3ccfdf3b055ab230b7
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-29 23:58:20 +0600

    Enhancement #17 : Home activity created again
    Again opened a new navigation drawer activity

---
+++ Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.abirhossain.nsu.fall2020.cse299.sec03.group03.ShopOnline.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
