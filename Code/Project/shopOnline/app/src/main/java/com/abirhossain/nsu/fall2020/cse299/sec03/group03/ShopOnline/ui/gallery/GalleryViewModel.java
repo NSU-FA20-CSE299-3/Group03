aa9ce6c967f297c647d32f13245b0f584c896225
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-29 17:17:39 +0600

    Enhancement #17 : New activity created
    A new activity with navigation draawer was created

---
+++ Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package com.abirhossain.nsu.fall2020.cse299.sec03.group03.ShopOnline.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
