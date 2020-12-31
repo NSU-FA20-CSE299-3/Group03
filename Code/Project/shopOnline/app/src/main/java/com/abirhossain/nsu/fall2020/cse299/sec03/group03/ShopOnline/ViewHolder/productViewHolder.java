d269ad1ed95e3810ea86dfab6e629f307da49e7f
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-01-01 03:39:37 +0600

    Enhancement #24 : New ViewHolder Class Created
    New ViewHolder Class Created

---
+++ Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/ViewHolder/productViewHolder.java
@@ -0,0 +1,18 @@
+package com.abirhossain.nsu.fall2020.cse299.sec03.group03.ShopOnline.ViewHolder;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class productViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public productViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
