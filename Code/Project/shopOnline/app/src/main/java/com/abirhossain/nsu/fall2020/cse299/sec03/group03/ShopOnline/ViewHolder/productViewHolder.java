16490c5150460e43fb7e5cc50d6e93b2d0c870d2
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-01-01 03:44:15 +0600

    Enhancement #24 : Initialized UI elements
    Initialized UI elements in viewHolder class

--- Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/ViewHolder/productViewHolder.java
+++ Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/ViewHolder/productViewHolder.java
@@ -1,14 +1,27 @@
 package com.abirhossain.nsu.fall2020.cse299.sec03.group03.ShopOnline.ViewHolder;
 
+import android.media.Image;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.abirhossain.nsu.fall2020.cse299.sec03.group03.ShopOnline.R;
+
 public class productViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public TextView ShowProductName,ShowProductDesc;
+    public ImageView ShowProductImage;
 
     public productViewHolder(@NonNull View itemView) {
         super(itemView);
+        ShowProductImage= itemView.findViewById(R.id.product_image_display);
+        ShowProductName = itemView.findViewById(R.id.product_name_display);
+        ShowProductDesc= itemView.findViewById(R.id.product_desc_display);
+        
+
     }
 
     @Override
