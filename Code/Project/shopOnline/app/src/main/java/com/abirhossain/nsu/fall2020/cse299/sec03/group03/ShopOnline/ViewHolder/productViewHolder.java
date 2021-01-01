facd06db3c7d73f414a9f802d42506d16789a8f0
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-01-01 19:28:36 +0600

    Enhancement #24 : New Ui element added
    New Ui element added to display product

--- Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/ViewHolder/productViewHolder.java
+++ Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/ViewHolder/productViewHolder.java
@@ -13,7 +13,7 @@ import com.abirhossain.nsu.fall2020.cse299.sec03.group03.ShopOnline.R;
 
 public class productViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-    public TextView ShowProductName,ShowProductDesc;
+    public TextView ShowProductName,ShowProductDesc,ShowProductPrice;
     public ImageView ShowProductImage;
     public ItemClickListener itemListener;
 
@@ -22,6 +22,7 @@ public class productViewHolder extends RecyclerView.ViewHolder implements View.O
         ShowProductImage= itemView.findViewById(R.id.product_image_display);
         ShowProductName = itemView.findViewById(R.id.product_name_display);
         ShowProductDesc= itemView.findViewById(R.id.product_desc_display);
+        ShowProductPrice = itemView.findViewById(R.id.product_price_display);
 
 
     }
