e05f61426c026b8be14d8c88bad8968d02ff42ed
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-01-04 20:09:05 +0600

    Enhancement #25 : New Activity created
    New Activity created

--- Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/MainActivity.java
+++ Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/MainActivity.java
@@ -84,6 +84,7 @@ public class MainActivity extends AppCompatActivity {
                             Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
                             loadingBar.dismiss();
                             Intent loginActivityIntent = new Intent(MainActivity.this, HomeActivity.class);
+                            Prevalent.onlineUsers = userData;
                             startActivity(loginActivityIntent);
                         }
                         else {
---
+++ Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/OrderDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.abirhossain.nsu.fall2020.cse299.sec03.group03.ShopOnline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_details);
+    }
+}
