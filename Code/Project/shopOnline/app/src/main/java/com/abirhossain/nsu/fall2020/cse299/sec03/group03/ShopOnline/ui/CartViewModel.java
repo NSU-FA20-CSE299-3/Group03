fdf8308999f6ef86868ab0811ba120151758e773
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-01-12 04:07:22 +0600

    Enhancement #28 : Cart fragment deleted
    Cart fragment deleted

--- Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/ui/CartViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.abirhossain.nsu.fall2020.cse299.sec03.group03.ShopOnline.ui;
-
-import androidx.lifecycle.ViewModel;
-
-public class CartViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
