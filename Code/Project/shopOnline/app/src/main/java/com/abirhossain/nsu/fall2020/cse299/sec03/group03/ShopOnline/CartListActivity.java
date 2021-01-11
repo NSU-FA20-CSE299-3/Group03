8bfa7828cd2cd55838a0ff50bebd578094d5d29f
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-01-11 21:28:35 +0600

    Enhancement #28 : New Activity created
    New Activity created named CartListActivity

---
+++ Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/CartListActivity.java
@@ -0,0 +1,14 @@
+package com.abirhossain.nsu.fall2020.cse299.sec03.group03.ShopOnline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CartListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_list);
+    }
+}
