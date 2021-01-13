7397b9d3fe9ea17b23f00ead42f0630bb4354dfa
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-01-14 05:17:27 +0600

    Enhancement #30 :  Ui initialized
    Ui initialized

--- Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/CheckoutActivity.java
+++ Code/Project/shopOnline/app/src/main/java/com/abirhossain/nsu/fall2020/cse299/sec03/group03/ShopOnline/CheckoutActivity.java
@@ -3,12 +3,23 @@ package com.abirhossain.nsu.fall2020.cse299.sec03.group03.ShopOnline;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CheckoutActivity extends AppCompatActivity {
+
+    private EditText coName,coPhone,coAddress,coCity;
+    private Button placeOrderBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+        coName = findViewById(R.id.coName);
+        coPhone = findViewById(R.id.coPhone);
+        coAddress = findViewById(R.id.coAddress);
+        coCity = findViewById(R.id.coCity);
+        placeOrderBtn = findViewById(R.id.placeOrderBtn);
+
     }
 }
